542e4f3dc74e100da3de00a4da845f1be290c098
xiaoanya <zf1111111@qq.com>
2019-04-20 16:28:30 +0800

    科技化工

---
+++ kj.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cstring>
+void test(){
+    char string[10];
+    char *str = "0123456789"; 
+    // strcpy(string, str);
+    printf("%s",string[0]);
+    printf("%d",str);
+}
+
+int main(){
+    // test();
+    // getchar();
+    return 0;
+}
